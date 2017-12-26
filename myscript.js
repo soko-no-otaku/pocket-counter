09641aacd947820f4e0ec75d2b685d140b14bda3
soko-no-otaku <soko.no.otaku@gmail.com>
2017-12-26 21:27:13 +0900

    init

---
+++ myscript.js
@@ -0,0 +1,12 @@
+function countArticles() {
+    var count = document.getElementsByClassName("item_content").length;
+    if (count >= 30) {
+        count = " (30+)";
+    } else {
+        count = " (" + count + ")";
+    }
+
+    document.getElementsByClassName("queue_title")[1].innerHTML += count;
+}
+
+setTimeout(countArticles, 1000);
